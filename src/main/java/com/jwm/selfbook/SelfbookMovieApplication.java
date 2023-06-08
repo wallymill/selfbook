c402f0754694a1e476bd609cf11c233fae961326
JWM <wallymill@yahoo.com>
2023-06-08 11:43:36 -0500

    JWM - adding initial Spring Boot configurations for Web, Data and H2

---
+++ src/main/java/com/jwm/selfbook/SelfbookMovieApplication.java
@@ -0,0 +1,13 @@
+package com.jwm.selfbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfbookMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfbookMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwm/selfbook/SelfbookMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwm.selfbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfbookMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
